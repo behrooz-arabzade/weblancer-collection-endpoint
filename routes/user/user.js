d98788420843c0879f5d5ebd94b5ee55f11d35e6
unknown <behrooz.arabzade@gmail.com>
2021-08-18 14:57:45 +0430

    add user route

--- routes/user/user.js
+++ routes/user/user.js
@@ -22,7 +22,7 @@ router.post('/login', async (req, res) => {
                 }
             });
 
-            console.log("login response 2")
+            console.log("login response 2", response.statusCode)
             if (response.statusCode === 200) {
                 let user = response.data.data.user;
 
