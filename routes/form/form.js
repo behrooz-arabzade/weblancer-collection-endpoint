c17a9b280041d63dfb81a88df2e0e22daa7ceb3c
unknown <behrooz.arabzade@gmail.com>
2021-08-19 13:27:11 +0430

    add form route

--- routes/form/form.js
+++ routes/form/form.js
@@ -1,5 +1,5 @@
 let express = require('express');
-const { createCollection, getAllCollections} = require('weblancer-collection');
+const { createCollection, getAllCollections, updateSchema} = require('weblancer-collection');
 let router = express.Router();
 let Response = require('../../utils/response');
 
@@ -23,17 +23,56 @@ router.post('/install', async (req, res) => {
         return;
     }
 
-    let {success: createSuccess, error: createError, errorStatusCode: createErrorStatusCode} =
+    let {success: createSuccess, collections: createCollections, error: createError, errorStatusCode: createErrorStatusCode} =
         await createCollection("contacts", "Contacts",
             "", "Form App", {}, true);
 
-    if (createSuccess) {
+    if (!createSuccess) {
+        res.status(createErrorStatusCode || 500).json(
+            new Response(false, {}, createError).json()
+        );
+        return;
+    }
+
+    let newCollection = createCollections.find(c => c.name === "contacts");
+    let schema = {
+        ...newCollection.schema,
+        submissionTime: {
+            weblancerType: "datetime",
+            order: 1,
+            name: "Submission Time"
+        },
+        firstName: {
+            weblancerType: "text",
+            order: 2,
+            name: "First Name"
+        },
+        lastName: {
+            weblancerType: "text",
+            order: 3,
+            name: "Last Name"
+        },
+        email: {
+            weblancerType: "text",
+            order: 4,
+            name: "Email"
+        },
+        Phone: {
+            weblancerType: "text",
+            order: 5,
+            name: "Phone"
+        },
+    };
+    let {success: updateSuccess, error: updateError, errorStatusCode: updateErrorStatusCode} =
+        await updateSchema(newCollection.name, schema);
+
+    if (updateSuccess) {
         res.status(200).json(
             new Response(true).json()
         );
     } else {
-        res.status(createErrorStatusCode || 500).json(
-            new Response(false, {}, createError).json()
+        res.status(updateErrorStatusCode || 500).json(
+            new Response(false, {}, updateError).json()
         );
     }
 });
